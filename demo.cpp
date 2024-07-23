0eedf6992bd3327545c4b22ee4c9b1f77f7a8536
nagesh150 <nageshjankwade2@gmail.com>
2024-07-24 00:23:04 +0530

    Added practice related files

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 
 int main()
 {
-	cout << "Inside main function...\n";
+	vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    vector<int>::iterator it;
+    for(it = v.begin(); it < v.end(); it++)
+    {
+        cout << *it << endl;
+    }
 	return 0;
 }
---
+++ practice.py
@@ -0,0 +1,7 @@
+a = [10,20,30]
+sum = 0
+def calculation():
+    for i in a:
+        print(i)
+calculation()
+print(sum)
